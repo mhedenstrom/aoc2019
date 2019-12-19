050b9465d2f29b24877bddb64a6c32212bed158f
Marcus Hedenström <marcus.hedenstrom@gmail.com>
2019-12-19 21:24:30 +0100

    day 4

---
+++ 04/part1.js
@@ -0,0 +1,25 @@
+function validate(password) {
+    password = '' + password
+    var equalPair = false
+    for (let i = 1; i < password.length; i++) {
+        if (password.charAt(i) < password.charAt(i-1)) {
+            return false
+        }
+        if (!equalPair && password.charAt(i) == password.charAt(i-1)) {
+            equalPair = true
+        }
+    }
+    return equalPair
+}
+
+function validPasswordsCount(from, to) {
+    var count = 0
+    for (let i = from; i <= to; i++) {
+        if (validate(i)) {
+            count++
+        }
+    }
+    return count
+}
+
+console.log(validPasswordsCount(236491, 713787))
